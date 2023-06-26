990caeef829ea3cc214330134e683dad0350e26d
HieuLam <hieudien55@gmail.com>
2023-06-26 11:26:03 +0700

    JSON_import_success

--- repository.js
+++
@@ -1,9 +0,0 @@
-//create a getListItem()
-
-export function JSONFileFetcher(){
-    fetch("C:\Users\hieud\Downloads\MOCK_DATA.json")
-    .then((res)=>{
-    return res.json();
-    })
-    .then((data)=>console.log(data));
-}
---
+++ src/JSONFileStorage.js
@@ -0,0 +1,6 @@
+import JSONFileFetcher from "./JSONFileFetcher";
+
+const list_items = [JSONFileFetcher()];
+return list_items;
+
+export default JSONFileStorage;
